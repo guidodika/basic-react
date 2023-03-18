57e46275c2fccadd6fde97c8bddf0f05f5805a90
guidodika <guidodikafirguston.mail@gmail.com>
2023-03-18 23:54:28 +0700

    external style react

--- public/app.js
+++ public/app.js
@@ -1,10 +1,17 @@
 const root = document.querySelector("#root");
 console.log("Hello Guys Hai");
+
+// const element = (
+//   <div
+//     style={{
+//       width: 200,
+//       height: 200,
+//       backgroundColor: "blue",
+//     }}
+//   ></div>
+// );
+
 const element = /*#__PURE__*/React.createElement("div", {
-  style: {
-    width: 200,
-    height: 200,
-    backgroundColor: "blue"
-  }
+  className: "box"
 });
 ReactDOM.render(element, root);
--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const root = document.querySelector("#root");
 
 console.log("Hello Guys Hai");
 
-const element = (
-  <div
-    style={{
-      width: 200,
-      height: 200,
-      backgroundColor: "blue",
-    }}
-  ></div>
-);
+// const element = (
+//   <div
+//     style={{
+//       width: 200,
+//       height: 200,
+//       backgroundColor: "blue",
+//     }}
+//   ></div>
+// );
+
+const element = <div className="box"></div>;
 
 ReactDOM.render(element, root);
