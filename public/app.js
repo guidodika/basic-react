802bb49f0b6b8b29202d8dfe313982f453a3f1f1
guidodika <guidodikafirguston.mail@gmail.com>
2023-03-18 20:41:04 +0700

    inline style jsx

--- public/app.js
+++ public/app.js
@@ -1,20 +1,10 @@
 const root = document.querySelector("#root");
-//Rendering
-
 console.log("Hello Guys Hai");
-function tick() {
-  const element = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Jam Sekarang"), /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString()));
-  ReactDOM.render(element, root);
-
-  // const element = `
-  //   <div>
-  //     <h1>Jam Sekarang</h1>
-  //     <h2>${new Date().toLocaleTimeString()}</h2>
-  //   </div>
-  // `;
-  // root.innerHTML = element;
-}
-
-setInterval(function () {
-  tick();
-}, 1000);
+const element = /*#__PURE__*/React.createElement("div", {
+  style: {
+    width: 200,
+    height: 200,
+    backgroundColor: "blue"
+  }
+});
+ReactDOM.render(element, root);
--- src/app.js
+++ src/app.js
@@ -1,26 +1,15 @@
 const root = document.querySelector("#root");
-//Rendering
 
 console.log("Hello Guys Hai");
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Jam Sekarang</h1>
-      <h2>{new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  ReactDOM.render(element, root);
+const element = (
+  <div
+    style={{
+      width: 200,
+      height: 200,
+      backgroundColor: "blue",
+    }}
+  ></div>
+);
 
-  // const element = `
-  //   <div>
-  //     <h1>Jam Sekarang</h1>
-  //     <h2>${new Date().toLocaleTimeString()}</h2>
-  //   </div>
-  // `;
-  // root.innerHTML = element;
-}
-
-setInterval(function () {
-  tick();
-}, 1000);
+ReactDOM.render(element, root);
