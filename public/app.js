98f98b9c5c428efecea621be752fe071114f3eea
guidodika <guidodikafirguston.mail@gmail.com>
2023-03-21 00:17:50 +0700

    conditional rendering 3

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const root = document.querySelector("#root");
 function App() {
   const [login, setLogin] = React.useState(false);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Halaman Login"), /*#__PURE__*/React.createElement("p", null, login ? /*#__PURE__*/React.createElement("b", null, "Berhasil Login") : /*#__PURE__*/React.createElement("i", null, "Gagal Login")), /*#__PURE__*/React.createElement("button", {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Halaman Login"), /*#__PURE__*/React.createElement("p", null, login && /*#__PURE__*/React.createElement("b", null, "Berhasil Login")), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
       setLogin(true);
     }
