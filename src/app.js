180c83e9067069f9c96de40295410587d2f699b4
guidodika <guidodikafirguston.mail@gmail.com>
2023-03-21 20:28:28 +0700

    react dom manipulation

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ function App() {
       <h1>Halaman Login</h1>
       {/* jika tidak ingin ada kondisi else, bisa pakai && */}
       <p>{login && <b>Berhasil Login</b>}</p>
+      {/* <p>{login ? "Berhasil Login" : "Gagal Login"}</p> */}
       <button
         onClick={function () {
           setLogin(true);
