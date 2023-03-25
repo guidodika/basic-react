004bea165224c5d3a6bb0731162452dc7e0c0d11
guidodika <guidodikafirguston.mail@gmail.com>
2023-03-25 22:09:04 +0700

    to-do list bagian 2

--- public/app.js
+++ public/app.js
@@ -2,9 +2,18 @@ const root = document.querySelector("#root");
 function App() {
   const [activity, setActivity] = React.useState("");
   const [todos, setTodos] = React.useState([]);
+
+  //generate id agar bisa membedakan antar item state activity
+  //disini menggunakan tanggal untuk generate id
+  function generateId() {
+    return Date.now();
+  }
   function addToDoHandler(event) {
     event.preventDefault();
-    setTodos([...todos, activity]);
+    setTodos([...todos, {
+      id: generateId(),
+      activity: activity
+    }]);
     setActivity("");
   }
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "To Do List"), /*#__PURE__*/React.createElement("form", {
@@ -18,8 +27,8 @@ function App() {
     }
   }), /*#__PURE__*/React.createElement("button", null, "Tambahkan")), /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
     return /*#__PURE__*/React.createElement("li", {
-      key: todo
-    }, todo);
+      key: todo.id
+    }, todo.activity);
   })));
 }
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
