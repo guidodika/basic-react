b881fcc2813ccfa04cc27195f221010be5a5e361
guidodika <guidodikafirguston.mail@gmail.com>
2023-03-25 21:52:01 +0700

    to-do list bagian 1

--- public/app.js
+++ public/app.js
@@ -1,38 +1,25 @@
 const root = document.querySelector("#root");
-
-//data fetching dengan promises chaining
-// function App() {
-//   React.useEffect(function () {
-//     const getData = fetch("https://api.spaceflightnewsapi.net/v3/blogs")
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (response) {
-//         console.log(response);
-//       });
-//     console.log(getData);
-//   }, []);
-//   return <h1>Data Fetch</h1>;
-// }
-
-//data fetching dengan async await
-
 function App() {
-  const [news, setNews] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
-  React.useEffect(function () {
-    async function getData() {
-      const request = await fetch("https://api.spaceflightnewsapi.net/v3/blogs");
-      const response = await request.json();
-      setLoading(false);
-      setNews(response);
+  const [activity, setActivity] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+  function addToDoHandler(event) {
+    event.preventDefault();
+    setTodos([...todos, activity]);
+    setActivity("");
+  }
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "To Do List"), /*#__PURE__*/React.createElement("form", {
+    onSubmit: addToDoHandler
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    placeholder: "Ketikkan sesuatu",
+    value: activity,
+    onChange: function (event) {
+      setActivity(event.target.value);
     }
-    getData();
-  }, []);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Data Fetch"), loading ? /*#__PURE__*/React.createElement("i", null, "Loading") : /*#__PURE__*/React.createElement("ul", null, news.map(function (item) {
+  }), /*#__PURE__*/React.createElement("button", null, "Tambahkan")), /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
     return /*#__PURE__*/React.createElement("li", {
-      key: item.id
-    }, item.title);
+      key: todo
+    }, todo);
   })));
 }
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,50 +1,35 @@
 const root = document.querySelector("#root");
 
-//data fetching dengan promises chaining
-// function App() {
-//   React.useEffect(function () {
-//     const getData = fetch("https://api.spaceflightnewsapi.net/v3/blogs")
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (response) {
-//         console.log(response);
-//       });
-//     console.log(getData);
-//   }, []);
-//   return <h1>Data Fetch</h1>;
-// }
-
-//data fetching dengan async await
-
 function App() {
-  const [news, setNews] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
+  const [activity, setActivity] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
 
-  React.useEffect(function () {
-    async function getData() {
-      const request = await fetch(
-        "https://api.spaceflightnewsapi.net/v3/blogs"
-      );
-      const response = await request.json();
-      setLoading(false);
-      setNews(response);
-    }
-    getData();
-  }, []);
+  function addToDoHandler(event) {
+    event.preventDefault();
+
+    setTodos([...todos, activity]);
+    setActivity("");
+  }
 
   return (
     <>
-      <h1>Data Fetch</h1>
-      {loading ? (
-        <i>Loading</i>
-      ) : (
-        <ul>
-          {news.map(function (item) {
-            return <li key={item.id}>{item.title}</li>;
-          })}
-        </ul>
-      )}
+      <h1>To Do List</h1>
+      <form onSubmit={addToDoHandler}>
+        <input
+          type="text"
+          placeholder="Ketikkan sesuatu"
+          value={activity}
+          onChange={function (event) {
+            setActivity(event.target.value);
+          }}
+        />
+        <button>Tambahkan</button>
+      </form>
+      <ul>
+        {todos.map(function (todo) {
+          return <li key={todo}>{todo}</li>;
+        })}
+      </ul>
     </>
   );
 }
